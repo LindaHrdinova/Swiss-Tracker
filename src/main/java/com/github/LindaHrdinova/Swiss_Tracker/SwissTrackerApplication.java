878311430880691c62a5d5dfba428d376cca12fc
Linda Hrdinová <hrlin@seznam.cz>
2025-01-08 17:16:00 +0100

    create a project Swiss Tracker

---
+++ src/main/java/com/github/LindaHrdinova/Swiss_Tracker/SwissTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.LindaHrdinova.Swiss_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwissTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwissTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/LindaHrdinova/Swiss_Tracker/SwissTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.LindaHrdinova.Swiss_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwissTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
